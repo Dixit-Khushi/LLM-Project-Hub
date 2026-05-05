908432751ea116716b54db269af462be94bd3289
Khushi Dixit <dixitkhushi288@gmail.com>
2026-05-05 13:21:07 +0530

    Implement weather fetching feature in app.py
    Added functionality to fetch and display current weather information using the Weather API.

--- project-1/app.py
+++ project-1/app.py
@@ -1,1 +1,17 @@
+# Importing requests package
+import requests
 
+# to get weather information of any location we use this url and change location keyword to any  location ...and make an api call through requests package
+url = "https://api.weatherapi.com/v1/current.json?key=your_api_key_here&q={location}"
+
+# this will be used to send a get request to url for retriving data... here parameters and keyword arguments can also be added which are optional
+response = requests.get(url)
+
+# thi will conert raw text into a clean pythin dictionary which makes it easier to work with the code
+data = response.json()
+
+# if we simply print data we will get information of everything that is present in the dictionary location's name region country latitude longitude etc..
+print(data)
+
+# so to get only the temprature ... we use data['current']['temp_c'] to get to the value of temprature itself and ignoring the other information
+print(f"Temperature: {data['current']['temp_c']}°C")
